67068f3f5c42d5ebc65483b475523bb71910f16f
judsonpaiva <judson.paiva@hotmail.com>
2022-04-17 23:29:32 +0100

    criação das classes Db e DbException

---
+++ src/db/DB.java
@@ -0,0 +1,40 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+	
+	private static Connection conn = null;
+	
+	
+	private static Properties loadProperties() {
+		try(FileInputStream fs = new FileInputStream("db.propriedades")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	
+}
