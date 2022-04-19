3266a05d7a2ac0e3f348b107a025d8fb199b5ebd
judsonpaiva <judson.paiva@hotmail.com>
2022-04-19 18:16:32 +0100

    apagando dados - f

--- src/app/Program2.java
+++ src/app/Program2.java
@@ -17,20 +17,31 @@ public class Program2 {
 		Connection conn = null;
 		PreparedStatement stm = null;
 		
+		String querySql = 
+				  "INSERT INTO SELLER "
+				+ "(NAME, EMAIL, BIRTHDATE, BASESALARY, DEPARTMENTID)"
+				+ "VALUES (?, ?, ?, ?, ?)";
+		
+		String querySql1 = 
+				  "INSERT INTO DEPARTMENT "
+				+ "(NAME)"
+				+ "VALUES (?)";
+		
 		try {
 			conn = DB.getConnection();
-			stm = conn.prepareStatement(
-					  "INSERT INTO SELLER "
-					+ "(NAME, EMAIL, BIRTHDATE, BASESALARY, DEPARTMENTID)"
-					+ "VALUES (?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
+			stm = conn.prepareStatement(querySql1);
+			
+			stm.setString(1, "D2");
+			//stm.setString(2, "D2");
+			/*
 			stm.setString(1, "Judson Paiva");
 			stm.setString(2, "judson.paiva@isptec.co.ao");
 			stm.setDate(3, new java.sql.Date(sdf.parse("12/05/1900").getTime()));
 			stm.setDouble(4, 3000000.0);
 			stm.setInt(5, 4);
+			*/
 			int rowsAffected = stm.executeUpdate();
-			
+			/*
 			if(rowsAffected > 0) {
 				ResultSet rs = stm.getGeneratedKeys();
 				while(rs.next()) {
@@ -40,8 +51,9 @@ public class Program2 {
 			}else {
 				System.out.println("No rown affected!");
 			}
+			*/
 			
-		}catch(SQLException | ParseException e) {
+		}catch(SQLException /*| ParseException*/ e) {
 			e.printStackTrace();
 		} finally {
 			DB.closeStatement(stm);
--- src/app/Program4.java
+++ src/app/Program4.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program4 {
 
@@ -17,18 +18,16 @@ public class Program4 {
 			conn = DB.getConnection();
 			
 			st = conn.prepareStatement(
-					  "UPDATE SELLER "
-					+ "SET BASESALARY = BASESALARY + ? "
+					  "DELETE FROM DEPARTMENT "
 					+ "WHERE "
-					+ "(DEPARTMENTID = ?)");
+					+ "ID = ?");
 			
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+			st.setInt(1, 2);
 			
-			int rowsAffected = st.executeUpdate();
-			System.out.println("Linhas afectadas = "+ rowsAffected);
+			int rows = st.executeUpdate();
+			System.out.println("Linhas afectadas = "+ rows);
 		}catch(SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
